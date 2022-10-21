8d42659500a5a01872d8f7db4f6d8c3636b3c9f9
hyunwoomemo <105469077+hyunwoomemo@users.noreply.github.com>
2022-10-21 12:14:23 +0900

    서브모듈 삭제

---
+++ 01-hello-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Hello from './MyComponent1';
+import World from './MyComponent2';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello React</h1>
+      <Hello/>
+      <World/>
+    </div>
+  );
+}
+
+export default App;
